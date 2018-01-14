0a2ccb6e88b4cf0af8548f11c951109b0dbd6321
AresX <aresx@protonmail.com>
2018-01-14 23:42:40 +0800

    'cdn'

---
+++ files/fade.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('body').css('display','none');$('body').fadeIn(450);$('a').click(function(){event.preventDefault();newLocation=this.href;$('body').fadeOut(200,newpage);});function newpage(){window.location=newLocation;}});
